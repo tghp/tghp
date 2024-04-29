44ea8451adf8eba7ea4427f0db6181da306e0c0c
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2024-04-29 14:59:52 +0100

    Fixed HTML entity

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,9 +4,10 @@ import { Divider } from "@/components/Divider";
 export default function About() {
   return (
     <Main.Container>
-         <Main.OverCanvas className="px-10 py-12 [font-stretch:130%] font-light">
+      <Main.OverCanvas className="px-10 py-12 [font-stretch:130%] font-light">
         <div className="prose prose-base prose-xl max-w-[65rem]">
-If you've got a project in mind please do not hesitate to get in touch - we'd love to chat to you about it.
+          If you&lsquo;ve got a project in mind please do not hesitate to get in
+          touch - we&lsquo;d love to chat to you about it.
         </div>
       </Main.OverCanvas>
       <Main.OverCanvas className="px-10 py-12 [font-stretch:130%] font-light">
