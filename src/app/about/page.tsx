f8fcc7754a7b7f185b222559b97268251e18b16c
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2024-04-29 15:01:12 +0100

    Housekeeping - corrected formatting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,28 +7,29 @@ export default function About() {
       <div className="ml-[--gutter-first] border-l border-teal-dark">
         <Main.OverCanvas className="pt-12 [font-stretch:130%] font-light">
           <div className="prose prose-base prose-xl max-w-[65rem]">
-            We design solutions that work for you. Throughout the whole process 
-            we make sure this is always at the forefront of all decision making. 
-            We make sure our solutions are always best in class and adaptive, 
-            ensuring we keep up to date with current trends and new technologies.
+            We design solutions that work for you. Throughout the whole process
+            we make sure this is always at the forefront of all decision making.
+            We make sure our solutions are always best in class and adaptive,
+            ensuring we keep up to date with current trends and new
+            technologies.
           </div>
         </Main.OverCanvas>
         <Divider />
         <Main.OverCanvas className="pt-12 [font-stretch:130%] font-light">
           <div className="prose prose-base prose-xl max-w-[65rem]">
-            Our London based team is made up of exceptional developers, 
-            techincal project managers and digital experts who thrive on the 
-            challenge of bringing your digital visions to life. 
+            Our London based team is made up of exceptional developers,
+            techincal project managers and digital experts who thrive on the
+            challenge of bringing your digital visions to life.
           </div>
         </Main.OverCanvas>
         <Divider />
         <Main.OverCanvas className="pt-12 [font-stretch:130%] font-light">
           <div className="prose prose-base prose-xl max-w-[65rem]">
-            We&apos;ve been designing and building bespoke sites and
-            handling custom integrations since 2010 so you&apos;re in safe
-            hands. We pride ourselves in producing and maintaining high quality,
-            user friendly, functional code with clear documentation and we
-            always hit our deadlines. 
+            We&apos;ve been designing and building bespoke sites and handling
+            custom integrations since 2010 so you&apos;re in safe hands. We
+            pride ourselves in producing and maintaining high quality, user
+            friendly, functional code with clear documentation and we always hit
+            our deadlines.
           </div>
         </Main.OverCanvas>
         <Divider />
